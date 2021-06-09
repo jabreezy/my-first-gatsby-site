9670f6b779c62e4b3e0b623820961bce8c8463fe
John Abebrese <johnabebrese@MacBook-Pro.cust.communityfibre.co.uk>
2021-06-09 11:05:17 +0100

    added pages and page

--- src/components/pages.js
+++ src/components/pages.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Page from './page';
 
 export default class Pages extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const formId = this.props.data.formOrder[0];
+    const pages = this.props.data.forms[formId].paging.map((page) => <Page components={page} />);
+
     return (
       <>
         <h2>Pages</h2>
+        {pages}
       </>
     );
   }
